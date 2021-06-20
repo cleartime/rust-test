b62df956f53c4da18d03c511544cb8a2aabd8251
gxx <562606139@qq.com>
2021-06-20 14:26:03 +0800

    init

---
+++ test3/target/rls/debug/deps/test3-2a9b88917ed181ab.d
@@ -0,0 +1,5 @@
+/Users/gxx/Desktop/btc/rust-test/test3/target/rls/debug/deps/test3-2a9b88917ed181ab.rmeta: src/main.rs
+
+/Users/gxx/Desktop/btc/rust-test/test3/target/rls/debug/deps/test3-2a9b88917ed181ab.d: src/main.rs
+
+src/main.rs:
---
+++ test3/target/rls/debug/deps/test3-ead7571b7cdaa5e8.d
@@ -0,0 +1,5 @@
+/Users/gxx/Desktop/btc/rust-test/test3/target/rls/debug/deps/test3-ead7571b7cdaa5e8.rmeta: src/main.rs
+
+/Users/gxx/Desktop/btc/rust-test/test3/target/rls/debug/deps/test3-ead7571b7cdaa5e8.d: src/main.rs
+
+src/main.rs:
